9de9cc4bca7a384193af878d31a4fc4c628398e8
Anton Brisilin <brisilant@yandex.ru>
2026-08-02 19:05:15 +0300

    v1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("org.jetbrains.intellij.platform") version "2.10.2"
 }
 
-group = "org.example"
-version = "1.0.0"
+group = "com.kotea.companion"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
@@ -15,7 +15,7 @@ repositories {
 
 dependencies {
     intellijPlatform {
-        local(file("/home/anton/.local/share/JetBrains/Toolbox/apps/android-studio/"))
+        androidStudio("2025.3.1.1")
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
         bundledPlugin("com.intellij.java")
@@ -30,8 +30,23 @@ intellijPlatform {
             sinceBuild = "253"
         }
 
+        description = """
+            <b>KoTEA Companion</b> is an Android Studio plugin designed to improve Developer Experience in projects using the KoTEA architectural pattern.
+            The plugin simplifies navigation by creating a contextual bridge between the points of generation (Emission) and execution (Processing) of Events and Commands.
+        """.trimIndent()
+
         changeNotes = """
-            Initial version
+            <ul>
+                <li><b>1.1.1</b>
+                    <ul>
+                        <li>Updated plugin description and documentation.</li>
+                        <li>Added support for K2 Kotlin plugin mode.</li>
+                        <li>Improved gutter markers for Event/Command constructor calls.</li>
+                        <li>Added "Go to Processing" action for commands in Handlers.</li>
+                        <li>Performance optimizations for project-wide searches.</li>
+                    </ul>
+                </li>
+            </ul>
         """.trimIndent()
     }
 }
